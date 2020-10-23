f9304602333dda63db0bb7875980e0feba02ed4e
yonlu <lss.emailbox@gmail.com>
2020-10-23 03:07:52 -0400

    chore: updated week 9 lab

--- Week 9/Trivia/main.cpp
+++ Week 9/Trivia/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 bool verifyAnswer(bool, bool);
 bool getUserAnswer();
-void askForAnswer(int);
 
 int main() {
     string questions[4];
@@ -23,51 +22,75 @@ int main() {
         questions[i] = question;
 
         // Ask user for the corresponding answer, and validate it
-        string answer;
-        askForAnswer(i+1);
-        getline(cin, answer);
-
-        bool flagIsTriggered; // Keep looping until flag is triggered
-        do {
-            if (!(answer.compare("T") && answer.compare("t") && answer.compare("True") && answer.compare("true"))) {
-                flagIsTriggered = false;
-                answers[i] = true;
-            }
-            else if(!(answer.compare("F") && answer.compare("f") && answer.compare("False") && answer.compare("false"))) {
-                flagIsTriggered = false;
-                answers[i] = false;
-            } else {
-                flagIsTriggered = true;
-                cout << "Invalid input, try again!" << endl;
-                askForAnswer(i+1);
-                getline(cin, answer);
-            }
-        } while(flagIsTriggered);
+        answers[i] = getUserAnswer();
     }
-
 
     cout << endl;
     cout << "Let's play the game!" << endl;
     cout << endl;
 
-    string input;
+    string  answer;
     for (int i = 0; i < 4; i++) {
        cout << "Question " << i+1 << ":" << endl;
        cout << questions[i] << endl;
 
        cout << "Please enter a guess: (T, t, True, true, F, f, False, false):" << endl;
-       cin >> input;
+       cin >> answer;
+        bool flagIsTriggered; // Keep looping until flag is triggered
+        do {
+            if (!(answer.compare("T") && answer.compare("t") && answer.compare("True") && answer.compare("true"))) {
+                flagIsTriggered = false;
+                if(verifyAnswer(true, answers[i])) {
+                    score++;
+                }
+            }
+            else if(!(answer.compare("F") && answer.compare("f") && answer.compare("False") && answer.compare("false"))) {
+                flagIsTriggered = false;
+                if(verifyAnswer(false, answers[i])) {
+                    score++;
+                }
+            } else {
+                flagIsTriggered = true;
+                cout << "Invalid input, try again!" << endl;
+                cout << "Please enter a guess  (T, t, True, true, F, f, False, false):" << endl;
+                getline(cin, answer);
+            }
+        } while(flagIsTriggered);
     }
+
+    cout << "You guessed " << score << " out of 4 questions correctly!" << endl;
 
     return 0;
 }
 
-void askForAnswer(int index) {
-    cout << "Please enter Answer " << index << " (T, t, True, true, F, f, False, false): " << endl;
-}
-
 bool getUserAnswer() {
     string answer;
-    cout << "Please enter Answer " << index << " (T, t, True, true, F, f, False, false): " << endl;
-    cin >> answer;
+
+    static int index = 1;
+
+    cout << "Please enter Answer " << index << " (T, t, True, true, F, f, False, false):" << endl;
+    getline(cin, answer);
+
+    bool flagIsTriggered; // Keep looping until flag is triggered
+    do {
+        if (!(answer.compare("T") && answer.compare("t") && answer.compare("True") && answer.compare("true"))) {
+            flagIsTriggered = false;
+            index++;
+            return true;
+        }
+        else if(!(answer.compare("F") && answer.compare("f") && answer.compare("False") && answer.compare("false"))) {
+            flagIsTriggered = false;
+            index++;
+            return false;
+        } else {
+            flagIsTriggered = true;
+            cout << "Invalid input, try again!" << endl;
+            cout << "Please enter Answer " << index << " (T, t, True, true, F, f, False, false):" << endl;
+            getline(cin, answer);
+        }
+    } while(flagIsTriggered); // clion is being dumb
+}
+
+bool verifyAnswer(bool userAnswer, bool realAnswer) {
+    return userAnswer == realAnswer;
 }
